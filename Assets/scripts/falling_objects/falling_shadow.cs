26f7c25d081d81cf508423d2429c25a71faab575
G7OSTIQ <106563737+G7OSTIQ@users.noreply.github.com>
2026-05-21 23:05:57 +0200

    small bug on falling objects

--- Assets/scripts/falling_objects/falling_shadow.cs
+++ Assets/scripts/falling_objects/falling_shadow.cs
@@ -10,7 +10,11 @@ public class falling_shadow : MonoBehaviour
     void Start()
     {
         Vector3 shadowpos = new Vector3(transform.position.x, groundY + 0.01f, transform.position.z);
-        shadow = Instantiate(shadowprefab, shadowpos, Quaternion.identity);
+    shadow = Instantiate(shadowprefab, shadowpos, Quaternion.identity);
+
+    float height = transform.position.y - groundY;
+    float scalesize = Mathf.Clamp(3f - (height / 10f), 0.5f, 3f);
+    shadow.transform.localScale = new Vector3(scalesize, 0.1f, scalesize);
     }
 
     void Update()
